2f3975ab0c4a053ca3f792fca3b5352e463afd12
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-05-06 15:27:12 -0300

    se crearon componentes para el header y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,10 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <header className="py-5 header">
-        <div className="container-xl">
-          <div className="row justify-content-center justify-content-md-between">
-            <div className="col-8 col-md-3">
-              <a href="index.html">
-                <img
-                  className="img-fluid"
-                  src="./public/assets/logo.svg"
-                  alt="imagen logo"
-                />
-              </a>
-            </div>
-            <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-              <div className="carrito">
-                <img
-                  className="img-fluid"
-                  src="./public/img/carrito.png"
-                  alt="imagen carrito"
-                />
-
-                <div id="carrito" className="bg-white p-3">
-                  <p className="text-center">El carrito esta vacio</p>
-                  <table className="w-100 table">
-                    <thead>
-                      <tr>
-                        <th>Imagen</th>
-                        <th>Nombre</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>
-                          <img
-                            className="img-fluid"
-                            src="./public/img/guitarra_02.jpg"
-                            alt="imagen guitarra"
-                          />
-                        </td>
-                        <td>SRV</td>
-                        <td className="fw-bold">$299</td>
-                        <td className="flex align-items-start gap-4">
-                          <button type="button" className="btn btn-dark">
-                            -
-                          </button>
-                          1
-                          <button type="button" className="btn btn-dark">
-                            +
-                          </button>
-                        </td>
-                        <td>
-                          <button className="btn btn-danger" type="button">
-                            X
-                          </button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-
-                  <p className="text-end">
-                    Total pagar: <span className="fw-bold">$899</span>
-                  </p>
-                  <button className="btn btn-dark w-100 mt-3 p-2">
-                    Vaciar Carrito
-                  </button>
-                </div>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
@@ -231,6 +162,7 @@ function App() {
               </button>
             </div>
           </div>
+
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
               <img
@@ -251,6 +183,7 @@ function App() {
               </button>
             </div>
           </div>
+
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
               <img
@@ -273,6 +206,7 @@ function App() {
               </button>
             </div>
           </div>
+
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
               <img
@@ -295,6 +229,7 @@ function App() {
               </button>
             </div>
           </div>
+
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
               <img
@@ -315,6 +250,7 @@ function App() {
               </button>
             </div>
           </div>
+
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
               <img
@@ -338,13 +274,7 @@ function App() {
         </div>
       </main>
 
-      <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-          <p className="text-white text-center fs-4 mt-4 m-md-0">
-            GuitarLA - Todos los derechos Reservados
-          </p>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
