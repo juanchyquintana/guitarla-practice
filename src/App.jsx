30385abeede6922b8b054872c72019cff9ae96c6
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-05-10 11:08:31 -0300

    se aplicó useMemo y se crearon las funciones para borrar un elemento del carrito, sumar y restar un producto

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,13 @@ function App() {
   const [data, setData] = useState(db);
   const [cart, setCart] = useState([]);
 
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+
   function addToCart(item) {
     const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
     if (itemExists >= 0) {
+      if(cart[itemExists].quantity >= MAX_ITEMS) return
       const updateCart = [...cart];
       updateCart[itemExists].quantity++;
       setCart(updateCart);
@@ -20,10 +24,43 @@ function App() {
     }
   }
 
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter(guitar => guitar.id !== id))
+  }
+
+  function increaseQuantity(id) {
+    const updateCart = cart.map( item => {
+      if(item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
+  function decreaseQuantity(id) {
+    const updateCart = cart.map( item => {
+      if(item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
   return (
     <>
       <Header 
         cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
       />
 
       <main className="container-xl mt-5">
